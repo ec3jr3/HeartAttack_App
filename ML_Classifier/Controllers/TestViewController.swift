d257d36faa016a97ba3f93a30dd3e78d290d853b
Andres <eacc9999@outlook.com>
2021-10-20 17:00:40 -0500

    cambie las variables de "var" a "let"

--- ML_Classifier/Controllers/TestViewController.swift
+++ ML_Classifier/Controllers/TestViewController.swift
@@ -16,6 +16,12 @@ class TestViewController: UIViewController {
     
     
     @IBOutlet var nameForTestL : UILabel!
+    @IBOutlet var ResultsTitile: UILabel!{
+        didSet{
+            ResultsTitile.layer.cornerRadius = 8
+            ResultsTitile.clipsToBounds = true
+        }
+    }
     
     @IBOutlet var mapButton: UIButton!
     
@@ -26,21 +32,20 @@ class TestViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = false
         
         //get the user information
-        //nameForTestL.text = personTest.name
         
-        var age = personTest.edad
-        var sex = personTest.sexo
-        var ches_pain = personTest.chest_pain_type
-        var bd = personTest.bp
-        var cholesterol = personTest.cholesterol
-        var fbs = personTest.fbs_over_120
-        var ekg = personTest.ekg_results
-        var max_hr = personTest.max_hr
-        var exercise_angina = personTest.exercise_angina
-        var st_depression = personTest.st_depressionL
-        var slope_st = personTest.slope_of_st
-        var num_vessels = personTest.number_of_vessels_fluro
-        var thallium = personTest.thallium
+        let age = personTest.edad
+        let sex = personTest.sexo
+        let ches_pain = personTest.chest_pain_type
+        let bd = personTest.bp
+        let cholesterol = personTest.cholesterol
+        let fbs = personTest.fbs_over_120
+        let ekg = personTest.ekg_results
+        let max_hr = personTest.max_hr
+        let exercise_angina = personTest.exercise_angina
+        let st_depression = personTest.st_depressionL
+        let slope_st = personTest.slope_of_st
+        let num_vessels = personTest.number_of_vessels_fluro
+        let thallium = personTest.thallium
         
         guard let prediction = try? mModel.prediction(Age: Double(age)!, Sex: Double(sex)!, Chest_pain_type: Double(ches_pain)!, BP: Double(bd)!, Cholesterol: Double(cholesterol)!, FBS_over_120: Double(fbs)!, EKG_results: Double(ekg)!, Max_HR: Double(max_hr)!, Exercise_angina: Double(exercise_angina)!, ST_depression: Double(st_depression)!, Slope_of_ST: Double(slope_st)!, Number_of_vessels_fluro: Double(num_vessels)!, Thallium: Double(thallium)!)
         else {
@@ -49,8 +54,7 @@ class TestViewController: UIViewController {
         print(prediction.Heart_DiseaseProbability)
        // dismiss(animated: true, completion: nil)
         
-        print(age)
-        print(sex)
+        
         
         //If the person got the results from the test a botton will apear to show the map for
         //nearest hospitals
@@ -63,17 +67,5 @@ class TestViewController: UIViewController {
         }
         
     }
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
